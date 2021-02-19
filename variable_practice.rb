617771edd4505a277ee81c4714856036631c9754
Aliya Merali <merali4@gmail.com>
2021-02-19 15:38:08 -0700

    Declare variables for practice

---
+++ variable_practice.rb
@@ -0,0 +1,6 @@
+variable_string = "string"
+variable_int = 1
+variable_float = 4.5
+variable_boolean = true
+variable_array = [1, 2, 3, 4]
+variable_hash = {"one": 1, "two": 2, "three": 3, "four": 4}
