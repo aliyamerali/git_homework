05225dc592d3ac1de002676cdcc119fbe658b8ed
Aliya Merali <merali4@gmail.com>
2021-02-19 15:39:15 -0700

    Re-assign variables for practice

--- variable_practice.rb
+++ variable_practice.rb
@@ -1,6 +1,6 @@
-variable_string = "string"
-variable_int = 1
+variable_string = "this is a new definition for this variable"
+variable_int = 25
 variable_float = 4.5
-variable_boolean = true
+variable_boolean = false
 variable_array = [1, 2, 3, 4]
 variable_hash = {"one": 1, "two": 2, "three": 3, "four": 4}
